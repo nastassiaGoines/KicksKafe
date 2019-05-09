24b9b741141c79e42f1adf88cda21e278eff16e5
nastassiaGoines <stassian@gmail.com>
2019-05-09 17:47:04 -0400

    thursday

--- src/Midterm/Cash.java
+++ src/Midterm/Cash.java
@@ -1,5 +1,36 @@
 package Midterm;
 
+import java.util.Scanner;
+
 public class Cash extends Payment {
+
+	private double change;
+	private double amount;
+	
+	public double getChange() {
+		return change;
+	}
+	public void setChange(double change) {
+		this.change = change;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	@Override
+	public String processingPayment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+//	while(userInput>amount) {
+//		return change;
+//	}
+	
 
 }
