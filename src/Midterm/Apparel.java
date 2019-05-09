4bc5f1038902f5a56081d61e8e8a26d91b3c78af
alayna-shepherd <alayna0@icloud.comgit config --global user.email alayna0@icloud.com>
2019-05-09 15:51:01 -0400

    initial commit apparel

---
+++ src/Midterm/Apparel.java
@@ -0,0 +1,29 @@
+package Midterm;
+
+public class Apparel extends Items {
+
+	public void Sneakers(String name, String description, double price) {
+		name = "sneakers";
+		description = "The classic Nike style you love - with an added flair from the KicksKafe team.";
+		price = 84.95;
+
+	}
+
+	public void Tee(String name, String description, double price) {
+		name = "T - Shirt";
+		description = "Athletic comfort, everyday style.";
+		price = 34.95;
+	}
+
+	public void Sweatpants(String name, String description, double price) {
+		name = "Sweatpants";
+		description = "Premium jogger fit.";
+		price = 54.95;
+	}
+
+	public void Sweatshirt(String name, String description, double price) {
+		name = "Sweatshirt";
+		description = "Super soft blended fabric, with the KicksKafe style.";
+		price = 64.95;
+	}
+}
