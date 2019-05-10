7fa1ef37e30044e63bfc42604fd6270c93e81d1f
nastassiaGoines <stassian@gmail.com>
2019-05-10 09:48:58 -0400

    second commit

--- src/Midterm/Payment.java
+++ src/Midterm/Payment.java
@@ -1,52 +1,17 @@
 package Midterm;
 
-public class Payment {
+public abstract class Payment {
 
-	private double cash;
-	private long credit;
-	private long check;
+	private String processing;
+	
 	
 	public Payment() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Payment(double cash, long credit, long check) {
-		super();
-		this.cash = cash;
-		this.credit = credit;
-		this.check = check;
-	}
 
-	public double getCash() {
-		return cash;
-	}
-
-	public void setCash(double cash) {
-		this.cash = cash;
-	}
-
-	public long getCredit() {
-		return credit;
-	}
-
-	public void setCredit(long credit) {
-		this.credit = credit;
-	}
-
-	public long getCheck() {
-		return check;
-	}
-
-	public void setCheck(long check) {
-		this.check = check;
-	}
-
-	@Override
-	public String toString() {
-		return cash+","+credit+","+check;
-	}
-	
+	public abstract String processingPayment();
 	
 	
 	
